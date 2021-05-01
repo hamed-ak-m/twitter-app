5ea1b154b874ac3ce61b75e7f909921e92d41a37
hamed-akbarzadeh-m <akbarzadeh.m.hamed@protonmail.com>
2021-05-01 18:37:28 +0430

    leftSidebar done

--- src/components/lefSidebar/LeftSidebar.js
+++ src/components/lefSidebar/LeftSidebar.js
@@ -1,13 +1,100 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { ButtonBase, Divider, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 import Grid from "@material-ui/core/Grid";
-import ButtonBase from "@material-ui/core/ButtonBase";
+
+const Twitters = ({ name, username, img }) => {
+  const classes = useStyles();
+  return (
+    <ButtonBase className={classes.Button}>
+      <Grid container direction={"row"} className={classes.twitterItem}>
+        <img alt={""} src={img} style={{ width: "2.5rem", height: "2.5rem" }} />
+
+        <Grid
+          item
+          container
+          direction={"column"}
+          className={classes.twitterNameParent}
+        >
+          <Typography className={classes.twitterName}>{name}</Typography>
+          <Typography className={classes.twitterUsername}>
+            {username}
+          </Typography>
+        </Grid>
+      </Grid>
+    </ButtonBase>
+  );
+};
+
+const twitter = [
+  {
+    name: "Xiaomi",
+    username: "@xiaomi",
+    img: "images/xiaomi.png",
+  },
+  {
+    name: "Samsung",
+    username: "@samsung",
+    img: "images/samsung.png",
+  },
+  {
+    name: "بیل گیتس",
+    username: "@billgates",
+    img: "images/bil.png",
+  },
+  {
+    name: "مایک بای",
+    username: "@mike_IMC",
+    img: "images/mike.png",
+  },
+  {
+    name: "شرلی ونگ",
+    username: "@sherlyweng",
+    img: "images/shily.png",
+  },
+];
 
 const LeftSidebar = () => {
   const classes = useStyles();
 
-  return <div className={classes.root}> left side bar</div>;
+  return (
+    <div className={classes.root}>
+      <Grid container direction={"row-reverse"}>
+        <img
+          alt={""}
+          src={"images/user img.png"}
+          style={{ width: "max-content" }}
+        />
+
+        <Grid item container direction={"column"} className={classes.profText}>
+          <Typography className={classes.profName}>محمد مطواعی</Typography>
+          <Typography className={classes.profUsername}>
+            Mohammad.metvayi
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container direction={"column"} className={classes.twittersroot}>
+        <Typography className={classes.twittersListTitle}>
+          بهترین خبرنگاران
+        </Typography>
+        <Divider style={{ marginLeft: -24, marginRight: -24 }} />
+        {twitter.map((item, index) => {
+          return (
+            <React.Fragment>
+              <Twitters
+                name={item.name}
+                username={item.username}
+                img={item.img}
+              />
+              {index !== twitter.length - 1 && (
+                <Divider style={{ marginLeft: -24, marginRight: -24 }} />
+              )}
+            </React.Fragment>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 };
 
 export default LeftSidebar;
--- src/components/lefSidebar/styles.js
+++ src/components/lefSidebar/styles.js
@@ -1,10 +1,63 @@
 import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "blue",
+    backgroundColor: "white",
     width: "25%",
+    padding: "1.5rem 2rem",
   },
-});
+  twitterNameParent: {
+    width: "max-content",
+    direction: "rtl",
+    justifyContent: "center",
+    marginRight: "0.5rem",
+  },
+  twitterItem: {
+    margin: "0.6rem 0.5rem",
+    backgroundColor: "#f5f8fa",
+    width: "100%",
+  },
+  Button: {
+    borderTopRightRadius: "2rem",
+    borderBottomRightRadius: "2rem",
+    color: theme.palette.primary.main,
+  },
+  twitterName: {
+    fontSize: "0.9rem !important",
+    fontWeight: 550,
+    direction: "ltr !important",
+    color: "black",
+  },
+  twitterUsername: {
+    color: theme.palette.text.hint,
+    fontSize: "0.65rem",
+  },
+  profText: {
+    marginLeft: "0.5rem",
+    width: "max-content",
+    justifyContent: "center",
+    direction: "ltr !important",
+  },
+  profName: {
+    fontSize: "0.9rem",
+    fontWeight: 550,
+    direction: "ltr !important",
+  },
+  profUsername: {
+    color: theme.palette.text.hint,
+    fontSize: "0.75rem",
+  },
+  twittersroot: {
+    backgroundColor: "#f5f8fa",
+    marginTop: "2rem !important",
+    borderRadius: "1.5rem",
+    padding: "0.7rem 1.2rem",
+  },
+  twittersListTitle: {
+    fontWeight: "600 ",
+    fontSize: "1rem ",
+    padding: "0.6rem 0.1rem",
+  },
+}));
 
 export default useStyles;
